d68d83d294f15f91d4222682f4fe14419d3b17d7
bibobirdy17 <arwen0317@gmail.com>
2025-04-08 13:12:39 +0800

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-// index.js
-// 儲存來自 index.html 的 localStorage 資料處理邏輯
-window.addEventListener("DOMContentLoaded", () => {
-  const total = document.getElementById("total-price")?.innerText || "";
-  const summary = document.getElementById("order-summary-text")?.innerHTML || "";
-  localStorage.setItem("order-total", total);
-  localStorage.setItem("order-summary", summary);
-});
